c06bf2f7d46fa80d95d4749d94fd29c248d2c4e4
unknown <the.jack.turner@gmail.com>
2021-11-02 16:49:56 +0000

    Just set up initial file structure

---
+++ main.py
@@ -0,0 +1,22 @@
+
+import tkinter as tk
+from tkinter import ttk
+
+##       Food Planner        ##
+
+class FoodPlanner:
+    def __init__(self):
+        self.root = tk.Tk()
+
+        self.window_w = 1440
+        self.window_h = 900
+        self.screen_w = self.root.winfo_screenwidth() 
+        self.screen_h = self.root.winfo_screenheight()
+        self.window_x = (self.screen_w/2) - (self.window_w/2)
+        self.window_y = (self.screen_h/2) - (self.window_h/2)
+        self.root.geometry("%dx%d+%d+%d" % (self.window_w, self.window_h, self.window_x, self.window_y))
+
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    app = FoodPlanner()
