2fe97c698408730f7f3bec81c25a40b7066167ea
BaconBaguette <the.jack.turner@gmail.com>
2022-09-09 16:07:32 +0100

    Built basics of navigation pane

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import tkinter as tk
 from tkinter import ttk
+from navigationPane import NavigationPane
 
 ##      Food Planner      ##
 
@@ -8,13 +9,21 @@ class FoodPlanner:
     def __init__(self):
         self.root = tk.Tk()
 
-        self.window_w = 1440
-        self.window_h = 900
+        # Set up window geometry #
+        self.window_with = 1440
+        self.window_height = 900
         self.screen_w = self.root.winfo_screenwidth() 
         self.screen_h = self.root.winfo_screenheight()
-        self.window_x = (self.screen_w/2) - (self.window_w/2)
-        self.window_y = (self.screen_h/2) - (self.window_h/2)
-        self.root.geometry("%dx%d+%d+%d" % (self.window_w, self.window_h, self.window_x, self.window_y))
+        self.window_x = (self.screen_w/2) - (self.window_with/2)
+        self.window_y = (self.screen_h/2) - (self.window_height/2)
+        self.root.geometry("%dx%d+%d+%d" % (self.window_with, self.window_height, self.window_x, self.window_y))
+
+        # Start building window content #
+        self.root_frame = ttk.Frame(self.root)
+        self.root_frame.grid(column=0, row=0)
+
+        self.navigation_pane = NavigationPane(self.root_frame)
+        self.navigation_pane.grid(column=0, row=0)
 
         self.root.mainloop()
 
--- navigationPane.py
+++ navigationPane.py
@@ -3,3 +3,11 @@ import tkinter as tk
 from tkinter import ttk
 
 ##      Navigation Pane      ##
+class NavigationPane(ttk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.nav_pane = ttk.Frame(master)
+        self.nav_pane.grid(column=0, row=0)
+
+        self.plan_meals_button = ttk.Button(self.nav_pane, text="Plan Meals")
+        self.plan_meals_button.grid(column=0, row=0)
